310c4ff5d37bfa956c6927194cc982ba075ba960
Esteban Lalinde <estebanl@mill5.com>
2023-01-19 11:33:59 -0500

    remove the boilerplate code from the entity

--- src/Intrinio.Net/Model/SecurityType.cs
+++ src/Intrinio.Net/Model/SecurityType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.Serialization;
-using System.Text;
-using Newtonsoft.Json;
+﻿using System.Runtime.Serialization;
 
 namespace Intrinio.Net.Model
 {
@@ -11,7 +6,7 @@ namespace Intrinio.Net.Model
     /// The summary of a financial instrument representing shares of ownership in a publicly-traded company
     /// </summary>
     [DataContract]
-    public sealed partial class SecurityType : IEquatable<SecurityType>, IValidatableObject
+    public sealed partial class SecurityType
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SecurityType" /> class.
@@ -38,88 +33,5 @@ namespace Intrinio.Net.Model
         /// <value>The name of the Security</value>
         [DataMember(Name = "name", EmitDefaultValue = false)]
         public string Name { get; set; }
-
-        /// <summary>
-        /// Returns the string presentation of the object
-        /// </summary>
-        /// <returns>String presentation of the object</returns>
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            sb.Append("class SecurityType {\n");
-            sb.Append("  Code: ").Append(Code).Append('\n');
-            sb.Append("  Name: ").Append(Name).Append('\n');
-            sb.Append("}\n");
-            return sb.ToString();
-        }
-
-        /// <summary>
-        /// Returns the JSON string presentation of the object
-        /// </summary>
-        /// <returns>JSON string presentation of the object</returns>
-        public string ToJson()
-        {
-            return JsonConvert.SerializeObject(this, Formatting.Indented);
-        }
-
-        /// <summary>
-        /// Returns true if objects are equal
-        /// </summary>
-        /// <param name="obj">Object to be compared</param>
-        /// <returns>Boolean</returns>
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as SecurityType);
-        }
-
-        /// <summary>
-        /// Returns true if SecurityType instances are equal
-        /// </summary>
-        /// <param name="input">Instance of SecurityType to be compared</param>
-        /// <returns>Boolean</returns>
-        public bool Equals(SecurityType input)
-        {
-            if (input == null)
-                return false;
-
-            return
-                (
-                    Name == input.Name ||
-                    (Name != null &&
-                    Name.Equals(input.Name))
-                ) &&
-                (
-                    Code == input.Code ||
-                    (Code != null &&
-                    Code.Equals(input.Code))
-                );
-        }
-
-        /// <summary>
-        /// Gets the hash code
-        /// </summary>
-        /// <returns>Hash code</returns>
-        public override int GetHashCode()
-        {
-            unchecked // Overflow is fine, just wrap
-            {
-                int hashCode = 41;
-                if (Name != null)
-                    hashCode = hashCode * 59 + Name.GetHashCode();
-                if (Code != null)
-                    hashCode = hashCode * 59 + Code.GetHashCode();
-                return hashCode;
-            }
-        }
-
-        /// <summary>
-        /// To validate all properties of the instance
-        /// </summary>
-        /// <param name="validationContext">Validation context</param>
-        /// <returns>Validation Result</returns>
-        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-        {
-            yield break;
-        }
     }
 }
