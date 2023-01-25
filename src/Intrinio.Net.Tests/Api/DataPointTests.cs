00d1f6bbe4dd423a14d025b2ab0835eb987ab602
Esteban Lalinde <84730201+estebanl5@users.noreply.github.com>
2023-01-25 14:25:46 -0500

    Added datapoint endpoint (#102)

---
+++ src/Intrinio.Net.Tests/Api/DataPointTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Intrinio.Net.Tests.TestManager;
+using Assert = NUnit.Framework.Assert;
+
+namespace Intrinio.Net.Tests.Api
+{
+    [TestClass]
+    public class DataPointTests
+    {
+        private const string APPLE_TICKER = "AAPL";
+        private const string BAD_TICKER = "CRZYC";
+        private const string MARKET_CAP_TAG = "marketcap";
+
+        [TestMethod]
+        public async Task GetDataPointNumberSucceedAsync()
+        {
+            var companiesResponse = await IntrinioTestClient.GetDataPointNumberAsync(APPLE_TICKER, MARKET_CAP_TAG);
+            Assert.IsNotNull(companiesResponse);
+        }
+
+        [TestMethod]
+        public void GetDataPointNumberBadTickerAsync()
+        {
+            Assert.ThrowsAsync<IntrinioNetException>(
+                async () => await IntrinioTestClient.GetDataPointNumberAsync(BAD_TICKER, MARKET_CAP_TAG));
+        }
+    }
+}
