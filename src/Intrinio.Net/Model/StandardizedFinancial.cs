4e41ccf0455109d084552f473efe4983e09e0297
Esteban Lalinde <estebanl@mill5.com>
2023-02-20 12:19:04 -0500

    remove unused code

--- src/Intrinio.Net/Model/StandardizedFinancial.cs
+++ src/Intrinio.Net/Model/StandardizedFinancial.cs
@@ -11,7 +11,7 @@ namespace Intrinio.Net.Model;
 /// Professional-grade historical financial data for a Company. This data is standardized, cleansed and verified to ensure the highest quality data sourced directly from the XBRL financial statements. The primary purpose of standardized financials are to facilitate comparability across a single company’s fundamentals and across all companies fundamentals. For example, it is possible to compare total revenues between two companies as of a certain point in time, or within a single company across multiple time periods. This is not possible using the as-reported financial statements because of the inherent complexity of reporting standards.
 /// </summary>
 [DataContract]
-public partial class StandardizedFinancial : IEquatable<StandardizedFinancial>, IValidatableObject
+public partial class StandardizedFinancial : IEquatable<StandardizedFinancial>
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="StandardizedFinancial" /> class.
@@ -37,19 +37,6 @@ public partial class StandardizedFinancial : IEquatable<StandardizedFinancial>, 
     [DataMember(Name = "value", EmitDefaultValue = false)]
     public decimal? Value { get; set; }
 
-    /// <summary>
-    /// Returns the string presentation of the object
-    /// </summary>
-    /// <returns>String presentation of the object</returns>
-    public override string ToString()
-    {
-        var sb = new StringBuilder();
-        sb.Append("class StandardizedFinancial {\n");
-        sb.Append("  DataTag: ").Append(DataTag).Append("\n");
-        sb.Append("  Value: ").Append(Value).Append("\n");
-        sb.Append("}\n");
-        return sb.ToString();
-    }
 
     /// <summary>
     /// Returns the JSON string presentation of the object
@@ -108,15 +95,5 @@ public partial class StandardizedFinancial : IEquatable<StandardizedFinancial>, 
                 hashCode = hashCode * 59 + this.Value.GetHashCode();
             return hashCode;
         }
-    }
-
-    /// <summary>
-    /// To validate all properties of the instance
-    /// </summary>
-    /// <param name="validationContext">Validation context</param>
-    /// <returns>Validation Result</returns>
-    IEnumerable<System.ComponentModel.DataAnnotations.ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-    {
-        yield break;
     }
 }
